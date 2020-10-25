62db7e28c7f65c9f934f2cddaf5dc5ec1913cc02
Mariep441 <mariep441@hotmail.com>
2020-10-25 09:39:12 +0100

    SplashScreen

---
+++ app/src/main/java/org/wit/placemark/activities/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package org.wit.placemark.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, PlacemarkListActivity.class));
+        finish();
+    }
+}
